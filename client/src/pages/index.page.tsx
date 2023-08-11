bb05bab2ca3c5ac8bb36346d7bb7b93d6d819a34
Diego <ddvallejoj@gmail.com>
2023-08-10 18:30:20 -0600

    mensaje home

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -43,7 +43,7 @@ export function Page() {
           />
         </div>
         <Typography variant="h4" mt={2}>
-          {texts.global.shopname}
+          Luman Gentium 18:20 PM UAT
         </Typography>
         <Typography variant="h6" m={1}>
           {texts.homepage.title}
