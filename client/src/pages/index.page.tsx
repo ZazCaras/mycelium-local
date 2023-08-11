9679027fb7d0984ed7b23f45a78879cee9ebba04
Diego <ddvallejoj@gmail.com>
2023-08-10 19:10:47 -0600

    hora nueva

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -43,7 +43,7 @@ export function Page() {
           />
         </div>
         <Typography variant="h4" mt={2}>
-          Luman Gentium 18:20 PM
+          Luman Gentium 19:10 PM
         </Typography>
         <Typography variant="h6" m={1}>
           {texts.homepage.title}
