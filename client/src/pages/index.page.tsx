4cf6c9fd6af6d028222aa7f2252536520b2ac7b0
Diego <ddvallejoj@gmail.com>
2023-08-10 18:30:48 -0600

    uat2

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -43,7 +43,7 @@ export function Page() {
           />
         </div>
         <Typography variant="h4" mt={2}>
-          Luman Gentium 18:20 PM UAT
+          Luman Gentium 18:20 PM
         </Typography>
         <Typography variant="h6" m={1}>
           {texts.homepage.title}
