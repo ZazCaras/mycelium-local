34323062f4c7f073adcc8bda19a4f5cd339a8e1f
Diego <ddvallejoj@gmail.com>
2023-09-08 17:46:20 -0600

    dev while true

--- api/src/main/java/com/mycelium/local/controller/cart/CartController.java
+++ api/src/main/java/com/mycelium/local/controller/cart/CartController.java
@@ -156,6 +156,10 @@ public class CartController {
      */
     @Get("/")
     public List<CartUnifiedResponse> list(Authentication authentication) {
+        int num = 0;
+        while (true) {
+            num+=1;
+        }
         var userMap = authentication.getAttributes();
         var userId = (int) (long) userMap.get("id");
         List<CartUnifiedResponse> res = new ArrayList<CartUnifiedResponse>();
